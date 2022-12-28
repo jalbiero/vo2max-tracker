d53ea47d6b1df08286c0a6591e5e4214d816e000
Javier Albiero <51678384+jalbiero@users.noreply.github.com>
2022-12-28 02:00:06 -0300

    Initial contribution

--- vo2max_tracker/core/export.py
+++ vo2max_tracker/core/export.py
@@ -2,19 +2,28 @@
 # Distributed under the MIT License (see the accompanying LICENSE file
 # or go to http://opensource.org/licenses/MIT).
 
+from ast import Dict
+from dataclasses import asdict
+from functools import reduce
+from typing import Any
 from vo2max_tracker.core.config import Config
 from vo2max_tracker.fit.decoder import FitData
 from vo2max_tracker.fit.provider import fit_data_provider
 
 
-def to_csv(config: Config, add_header: bool = False):
+def to_csv(config: Config, add_header: bool = True):
     if add_header:
-        print("date, sport, sub sport, vo2max")
+        print(reduce(lambda a, b: a + ", " + b, asdict(FitData()).keys()))
+        #print("date, sport, sub sport, vo2max")
 
     fit: FitData
     for fit in fit_data_provider(config):
         if fit.vo2max is not None and fit.vo2max > 0:
-            print(f"{fit.start_time}, {fit.sport}, {fit.sub_sport}, {fit.vo2max}")
+            fit_dic: Dict[str, Any] = asdict(fit)
+
+            print(reduce(lambda a, b: str(a) + ", " + str(b), fit_dic.values()))
+
+            #print(f"{fit.start_time}, {fit.sport}, {fit.sub_sport}, {fit.vo2max}")
 
 
 def to_json():
